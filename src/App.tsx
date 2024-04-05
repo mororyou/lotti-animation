32666853708ca57f6f77e37da2b57bf8a1099a08
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-04-05 18:19:16 +0900

    :sparkles: Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Lottie from 'lottie-react'
+import groovyWalkAnimation from "./assets/bucketful.json";
+
+function App() {
+
+  return (
+    <>
+      <Lottie animationData={groovyWalkAnimation} />
+    </>
+  )
+}
+
+export default App
